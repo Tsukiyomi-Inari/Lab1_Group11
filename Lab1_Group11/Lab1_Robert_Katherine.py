9c65ec5a9887ea3b462c19334f272abc0293c041
DPNurple <dpnurple@gmail.com>
2021-10-01 15:16:53 -0400

    Brought code back to current level

--- Lab1_Group11/Lab1_Robert_Katherine.py
+++ Lab1_Group11/Lab1_Robert_Katherine.py
@@ -37,7 +37,6 @@ def validInputCheck(input):
      1. Null entry
      2. Non numeric characters
      3. Length exceeding 16
-
      if invalid, error message will be printed to
      the console and user will be promped again
     :return:
@@ -63,14 +62,37 @@ def validInputCheck(input):
     else:
         return True
 
+def when_even_index(card_number):
+    """
+    takes card_number list and finds each even index
+    multiplies it by 9 (via constant)
+        then if the result is greater than 9
+            subtracts the value of 9 from it
+    the final result is then inputted into a new list
+        """
+    # double every value at even intervals, if any result is greater than 9, subtract 9 from it(the doubled value)
+    even_element = []
+    for index in card_number:
+        # multiply each EVEN index by 9 and add to new list
+        even_element = index[-2::2] * GREATER_THAN
+        if even_element[index] > GREATER_THAN:
+            even_element[index] = even_element[index] - GREATER_THAN
+        else:
+            pass
+
 
 system("cls")
 system("Lab 1: Credit Card Validator - Group 11")
 # accept card number as string with/out spaces  from user
+
 input = input("Please enter a credit card number:").strip()
 
+
+#Call validation function
 validInputCheck(input)
 
+
+# Call function to convert the validated string to input each digit into separate indexe's in list
 stringToList(input)
 
 # remove rightmost digit(checking digit) from input number, excluding it from calculations
